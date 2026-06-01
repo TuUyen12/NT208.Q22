0b8f487b72279779c9faf442e3fada75b8e5a9de
TuUyen12 <lethituuyen1204@gmail.com>
2026-06-01 22:32:29 +0700

    feat: Update Mobile UI

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -10,7 +10,6 @@ import AuthCallback   from "./pages/AuthCallback";
 import DailyHoroscope from "./pages/DailyHoroscope";
 import Journal        from "./pages/Journal";
 import Profile        from "./pages/Profile";
-import Zodiac          from "./pages/Zodiac";
 
 function ProtectedRoute({ children }) {
   const { user, loading } = useAuth();
@@ -28,7 +27,6 @@ export default function App() {
           <Route path="/signup"        element={<SignUp />} />
           <Route path="/auth/callback" element={<AuthCallback />} />
           <Route path="/major-stars"   element={<MajorStars />} />
-          <Route path="/zodiac"        element={<Zodiac/> } />
           <Route path="/chatbot"          element={<ProtectedRoute><Chatbot /></ProtectedRoute>} />
           <Route path="/la-so"           element={<ProtectedRoute><LaSoTuVi /></ProtectedRoute>} />
           <Route path="/la-so-tu-vi"     element={<Navigate to="/la-so" replace />} />
